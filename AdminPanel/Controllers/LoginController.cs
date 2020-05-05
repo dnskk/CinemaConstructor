730ba733ff674bf87818eebc8d833ef14b0a1d6a
Dinar <dffaiskhanov@edu.hse.ru>
2020-05-06 01:18:46 +0500

    added login ui

---
+++ AdminPanel/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+    }
+}
