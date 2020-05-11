5fd73d88c997186eb181edf069f01a862fc3098d
Dinar <dffaiskhanov@edu.hse.ru>
2020-05-12 03:20:42 +0500

    added select and deselect buttons

---
+++ AdminPanel/wwwroot/js/hallScripts.js
@@ -0,0 +1,79 @@
+﻿function pad(n, width) {
+    n = n + "";
+    return n.length >= width ? n : new Array(width - n.length + 1).join("0") + n;
+}
+
+function addHighlighting() {
+    $(function () {
+        var isMouseDown = false,
+            isHighlighted;
+        $("#hallTable td")
+            .mousedown(function () {
+                isMouseDown = true;
+                $(this).toggleClass("highlighted");
+                isHighlighted = $(this).hasClass("highlighted");
+                return false; // prevent text selection
+            })
+            .mouseover(function () {
+                if (isMouseDown) {
+                    $(this).toggleClass("highlighted", isHighlighted);
+                }
+            })
+            .bind("selectstart",
+                function () {
+                    return false;
+                });
+
+        $(document)
+            .mouseup(function () {
+                isMouseDown = false;
+            });
+    });
+}
+
+function updateTable() {
+    row = document.getElementById("rows").value;
+    columns = document.getElementById("columns").value;
+
+    var divTable = document.getElementById("divTable");
+
+    var table = document.createElement("table");
+    table.id = "hallTable";
+    table.style.width = "100%";
+    table.style.height = "100%";
+    for (var i = 0; i < row; i++) {
+        var tr = document.createElement("tr");
+        for (var j = 0; j < columns; j++) {
+            var td = document.createElement("td");
+            td.id = i + "_" + j;
+            //td.appendChild(document.createTextNode(pad(i + 1, 2) + "-" + pad(j + 1, 2)));
+            tr.appendChild(td);
+        }
+
+        table.appendChild(tr);
+    }
+
+    divTable.innerHTML = "";
+    divTable.appendChild(table);
+    addHighlighting();
+}
+
+function tableSelectAll() {
+    for (var i = 0; i < row; i++) {
+        var tr = document.createElement("tr");
+        for (var j = 0; j < columns; j++) {
+            var td = document.getElementById(i + "_" + j);
+            td.classList.add("highlighted");
+        }
+    }
+}
+
+function tableDeselectAll() {
+    for (var i = 0; i < row; i++) {
+        var tr = document.createElement("tr");
+        for (var j = 0; j < columns; j++) {
+            var td = document.getElementById(i + "_" + j);
+            td.classList.remove("highlighted");
+        }
+    }
+}
